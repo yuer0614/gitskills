3ccca97e93192a0afe82eb22780dcf993df14022
System Administrator <root@liushuaideMacBook-Pro.local>
2019-01-17 09:26:58 +0800

    increase test.sh file

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Hello World !"
+url="http://c.biancheng.net"
+website1='hello ${url}'
+website2="hello ${url}"
+echo $website1
+echo $website2
+a=/Users/michael/gitskills
+echo $a
+echo ${a}/gitskills
+cd ${a}/gitskills
+pwd
+cat one.txt
